2f79ff86e9c3d0d3ce21a5e68f96dd95e0433ecc
Stwart de Assunção <stwart.tech@gmail.com>
2026-06-27 17:42:57 -0300

    calculo de salarios

--- calculo_salario.py
+++ calculo_salario.py
@@ -7,3 +7,21 @@
 """
 
 #QUEBRANDO O PROBLEMA EM PARTES
+#receber o salário bruto 
+#gratificação 5%
+#imposto 7%
+#bruto-imposto+gratificação 
+#salário líquido 
+
+#RECEBIMENTO DOS DADOS 
+bruto=float(input("\nQual seu salário\n"))
+
+#REALIZAÇÃO DOS CALCULOS 
+gratificacao=float(bruto*0.05)
+imposto=float(bruto*0.07)
+liquido=float(bruto-imposto+gratificacao)
+
+#SAÍDA DOS DADOS 
+print(f"\nSeu salário liquido é {liquido}")
+
+
